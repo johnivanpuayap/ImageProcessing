7777fdc539abaed61d5529b89996ed7bff0e4679
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-20 18:49:33 +0800

    Add User Interface

--- ImageProcessing/Form1.Designer.cs
+++ ImageProcessing/Form1.Designer.cs
@@ -28,13 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbLoaded = new System.Windows.Forms.PictureBox();
+            this.pbProcessed = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.activity1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.basicCopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inverseImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.histogramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoaded)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProcessed)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbLoaded
+            // 
+            this.pbLoaded.Location = new System.Drawing.Point(56, 48);
+            this.pbLoaded.Name = "pbLoaded";
+            this.pbLoaded.Size = new System.Drawing.Size(500, 500);
+            this.pbLoaded.TabIndex = 0;
+            this.pbLoaded.TabStop = false;
+            // 
+            // pbProcessed
+            // 
+            this.pbProcessed.Location = new System.Drawing.Point(629, 48);
+            this.pbProcessed.Name = "pbProcessed";
+            this.pbProcessed.Size = new System.Drawing.Size(500, 500);
+            this.pbProcessed.TabIndex = 1;
+            this.pbProcessed.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.activity1ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1182, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // activity1ToolStripMenuItem
+            // 
+            this.activity1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.basicCopyToolStripMenuItem,
+            this.grayToolStripMenuItem,
+            this.inverseImageToolStripMenuItem,
+            this.histogramToolStripMenuItem,
+            this.sepiaToolStripMenuItem});
+            this.activity1ToolStripMenuItem.Enabled = false;
+            this.activity1ToolStripMenuItem.Name = "activity1ToolStripMenuItem";
+            this.activity1ToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.activity1ToolStripMenuItem.Text = "Activity 1";
+            // 
+            // basicCopyToolStripMenuItem
+            // 
+            this.basicCopyToolStripMenuItem.Name = "basicCopyToolStripMenuItem";
+            this.basicCopyToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.basicCopyToolStripMenuItem.Text = "Basic Copy";
+            // 
+            // grayToolStripMenuItem
+            // 
+            this.grayToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.grayToolStripMenuItem.Name = "grayToolStripMenuItem";
+            this.grayToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.grayToolStripMenuItem.Text = "Gray Scale";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 563);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(844, 563);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processed Image";
+            // 
+            // inverseImageToolStripMenuItem
+            // 
+            this.inverseImageToolStripMenuItem.Name = "inverseImageToolStripMenuItem";
+            this.inverseImageToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.inverseImageToolStripMenuItem.Text = "Inverse Image";
+            // 
+            // histogramToolStripMenuItem
+            // 
+            this.histogramToolStripMenuItem.Name = "histogramToolStripMenuItem";
+            this.histogramToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.histogramToolStripMenuItem.Text = "Histogram";
+            // 
+            // sepiaToolStripMenuItem
+            // 
+            this.sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
+            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sepiaToolStripMenuItem.Text = "Sepia";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.changeFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // changeFileToolStripMenuItem
+            // 
+            this.changeFileToolStripMenuItem.Name = "changeFileToolStripMenuItem";
+            this.changeFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.changeFileToolStripMenuItem.Text = "Change File";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbProcessed);
+            this.Controls.Add(this.pbLoaded);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Image Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoaded)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProcessed)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbLoaded;
+        private System.Windows.Forms.PictureBox pbProcessed;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem activity1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem basicCopyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inverseImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem histogramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sepiaToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeFileToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- ImageProcessing/Form1.cs
+++ ImageProcessing/Form1.cs
@@ -12,9 +12,43 @@ namespace ImageProcessing
 {
     public partial class Form1 : Form
     {
+
+
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Image loadedImage;
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            
+            openFileDialog1.Title = "Choose an image";
+
+            openFileDialog1.InitialDirectory = @"C:\";
+
+            // add filter for files only
+            openFileDialog1.Filter = "Image files (*.bmp;*.jpg;*.jpeg;*.gif;*.png)|*.bmp;*.jpg;*.jpeg;*.gif;*.png|All files (*.*)|*.*";
+
+
+            // Show the dialog and check if the user clicked OK
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file's path
+                string selectedFilePath = openFileDialog1.FileName;
+
+                loadedImage = Image.FromFile(selectedFilePath);
+
+                // Display the Image File
+                Image scaledImage = ScaleImage
+                pbLoaded.Image = scaledImage;
+
+                // enable the Activity 1 Strip Menu Item
+                activity1ToolStripMenuItem.Enabled = true;
+            }
         }
     }
 }
